8fefdcb6a2572d4d2fcd69ea2217d544701f54cd
Git-Monke <javelasq2006@gmail.com>
2023-03-19 10:08:52 -0700

    Finished solving function

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
+const { readFile } = require("fs");
+
+const readline = require("readline").createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
 const WIN_CONDITIONS = [
   0b111000000, 0b000111000, 0b000000111, 0b100100100, 0b010010010, 0b001001001,
   0b100010001, 0b001010100,
@@ -120,15 +127,133 @@ class Position {
     score += this.board.score() * 10;
     return score;
   }
+
+  canPlay(s, i) {
+    if ((this.legalBoards & (0b1 << s)) == 0) {
+      return false;
+    }
+    if (!this.subboards[s].canPlay(i)) {
+      return false;
+    }
+    return true;
+  }
+
+  getLegalMoves() {
+    let moves = [];
+    let start = this.subboard != null ? P.subboard : 0;
+    let end = this.subboard != null ? P.subboard + 1 : 9;
+
+    for (let s = start; s < end; s++) {
+      for (let i = 0; i < 9; i++) {
+        if (P.canPlay(s, i)) {
+          moves.push([s, i]);
+        }
+      }
+    }
+
+    return moves;
+  }
 }
 
-let P = new Position();
-let iters = 1_000_000;
-let start = performance.now();
-console.profile();
-for (let i = 0; i < iters; i++) {
-  P.score();
+// 0 maximizing, 1 minimizing
+function minimax(P, depth, alpha = -Infinity, beta = Infinity) {
+  let board = P.board;
+  let player = P.player;
+
+  // Check for a win
+  if (board.checkWin(board.boards[1 - player])) {
+    let sign = 1 - player == 1 ? -1 : 1;
+    return 1_000_000 * sign;
+  }
+
+  // Check for a draw
+  if (board.legalBoards == 0) {
+    return 0;
+  }
+
+  // Check if the max search depth has been reached
+  if (depth == 0) {
+    return P.score();
+  }
+
+  let bestScore = player == 0 ? -Infinity : Infinity;
+  let start = P.subboard != null ? P.subboard : 0;
+  let end = P.subboard != null ? P.subboard + 1 : 9;
+
+  for (let s = start; s < end; s++) {
+    let subboard = P.subboards[s];
+
+    for (let i = 0; i < 9; i++) {
+      if (!subboard.canPlay(i)) {
+        continue;
+      }
+
+      let P2 = new Position(P);
+      P2.move(s, i);
+
+      let score = minimax(P2, depth - 1, alpha, beta);
+
+      if (player == 0) {
+        bestScore = Math.max(score, bestScore);
+        alpha = Math.max(bestScore, alpha);
+      } else {
+        bestScore = Math.min(score, bestScore);
+        beta = Math.min(bestScore, beta);
+      }
+
+      if (beta <= alpha) {
+        break;
+      }
+    }
+  }
+
+  return bestScore;
 }
-console.profileEnd();
-let time = performance.now() - start;
-console.log(`${(time / iters) * 1_000_000}ns per move`);
+
+function solve(P, depth) {
+  let moves = P.getLegalMoves();
+  let player = P.player;
+
+  let bestScore = player == 0 ? -Infinity : Infinity;
+  let bestMove = null;
+
+  for (let i = 0; i < moves.length; i++) {
+    let move = moves[i];
+    let P2 = new Position(P);
+    P2.move(move[0], move[1]);
+    let score = minimax(P2, depth - 1);
+
+    if (
+      (player == 0 && score > bestScore) ||
+      (player == 1 && score < bestScore)
+    ) {
+      bestScore = score;
+      bestMove = move;
+    }
+  }
+
+  return bestMove;
+}
+
+let depth = 8;
+let P = new Position();
+console.log(solve(P, depth));
+
+// P.move(4, 4);
+// let start = performance.now();
+// console.log(solve(P, depth));
+// let end = performance.now();
+// let time = end - start;
+// console.log(`Depth ${depth}: ${time.toFixed(2)}ms`);
+// console.log(`${n} nodes evaluted`);
+// console.log(`${((n / time) * 1000).toFixed()} nodes per second`);
+
+// let iters = 1_000_000;
+// let start = performance.now();
+// console.profile();
+// for (let i = 0; i < iters; i++) {
+//   P.score();
+// }
+// console.profileEnd();
+// let time = performance.now() - start;
+// console.log(`${(time / iters) * 1_000_000}ns per move`);
