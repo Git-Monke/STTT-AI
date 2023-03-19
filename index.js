9051ee8f697bd7444fa2fbc1dfbba94690b6993e
Git-Monke <javelasq2006@gmail.com>
2023-03-18 20:56:21 -0700

    Added move making and board cloning features

--- index.js
+++ index.js
@@ -1,69 +1,124 @@
 const WIN_CONDITIONS = [
-    0b111000000,
-    0b000111000,
-    0b000000111,
-    0b100100100,
-    0b010010010,
-    0b001001001,
-    0b100010001,
-    0b001010100
-]
+  0b111000000, 0b000111000, 0b000000111, 0b100100100, 0b010010010, 0b001001001,
+  0b100010001, 0b001010100,
+];
 
 function bitcount(b) {
-    let count = 0;
+  let count = 0;
 
-    while (b != 0) {
-        b = b & (b - 1);
-        count += 1;
-    }
+  while (b != 0) {
+    b = b & (b - 1);
+    count += 1;
+  }
 
-    return count;
+  return count;
 }
 
 class Board {
-    constructor() {
-        this.boards = [
-            0b000000000,
-            0b000000000
-        ]
+  constructor() {
+    this.boards = [0b000000000, 0b000000000];
+  }
+
+  moveWins(i, p) {
+    let board = this.boards[p] & (0b1 << i);
+    return this.checkWin(board);
+  }
+
+  canPlay(i) {
+    let mask = this.boards[0] | this.boards[1];
+    return (mask & (0b1 << i)) == 0;
+  }
+
+  move(i, p) {
+    this.boards[p] = this.boards[p] | (0b1 << i);
+    return this.checkWin(this.boards[p]);
+  }
+
+  score() {
+    let score = 0;
+
+    for (let i = 0; i < 8; i++) {
+      let x = bitcount(this.boards[0] & WIN_CONDITIONS[i]);
+      let o = -bitcount(this.boards[1] & WIN_CONDITIONS[i]);
+      let s = o + x;
+
+      if (s == 0) {
+        continue;
+      }
+
+      score += 10 ** (Math.abs(s) - 1) * (s < 0 ? -1 : 1);
     }
 
-    moveWins(i, p) {
-        let board = this.boards[p] & 0b1 << i;
-        return this.checkWin(board);
+    return score;
+  }
+
+  checkWin(board) {
+    for (let i = 0; i < 8; i++) {
+      let condition = WIN_CONDITIONS[i];
+      if ((board & condition) == condition) {
+        return true;
+      }
     }
 
-    canPlay(i) {
-        let mask = this.boards[0] | this.boards[1];
-        return (mask & (0b1 << i)) == 0;
-    }
+    return false;
+  }
 
-    move(i, p) {
-        this.boards[p] = this.boards[p] | 0b1 << i;
-    }
-
-    score() {
-        let score = 0;
-
-        for (let i = 0; i < 8; i++) {
-            let x = bitcount(this.boards[0] & WIN_CONDITIONS[i]);
-            let o = -bitcount(this.boards[1] & WIN_CONDITIONS[i]);
-            let s = o + x;
-
-            if (s == 0) { continue }
-            
-            score += (10 ** (Math.abs(s) - 1)) * (s < 0 ? -1 : 1)
-        }
-
-        return score;
-    }
-
-    checkWin(board) {
-        for (let i = 0; i < 8; i++) {
-            let condition = WIN_CONDITIONS[i];
-            if ((board & condition) == condition) { return true }
-        }
-
-        return false;
-    }
+  clone() {
+    let board = new Board();
+    board.boards = this.boards.slice();
+    return board;
+  }
 }
+
+class Position {
+  constructor(P) {
+    if (P) {
+      this.subboards = [];
+      for (let i = 0; i < 9; i++) {
+        this.subboards[i] = P.subboards[i].clone();
+      }
+      this.board = P.board.clone();
+      this.subboard = P.subboard;
+      this.legalBoards = P.legalBoards;
+      this.player = P.player;
+      return;
+    }
+
+    this.subboards = new Array(9).fill(null).map((_) => new Board());
+    this.legalBoards = 0b111111111;
+    this.subboard = null;
+    this.player = 0;
+    this.board = new Board();
+  }
+
+  move(s, i) {
+    s = this.subboard || s;
+    let p = this.player;
+    let r1 = this.subboards[s].move(i, p);
+    if (r1) {
+      this.board.move(s, p);
+      this.legalBoards = this.legalBoards & ~(0b1 << s);
+    }
+    this.player = 1 - this.player;
+    let mask = 0b1 << i;
+    this.subboard = (this.legalBoards & mask) == mask ? i : null;
+  }
+}
+
+function randint() {
+  return Math.floor(Math.random() * 9);
+}
+
+let P = new Position();
+let iters = 1_000_000;
+let moves = [];
+
+let start = performance.now();
+console.profile();
+for (let i = 0; i < iters; i++) {
+  let P2 = new Position(P);
+  P2.move(0, 0);
+}
+console.profileEnd();
+let time = performance.now() - start;
+console.log(`${iters / time}`);
